be8ebeb1bee11d69c247d7e069d0efb13298efc8
Thomas Johnson <thomasij813@gmail.com>
2019-09-24 13:53:45 -0700

    add semantic-ui

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,7 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>Hello World</div>
+    return <div className="ui text container">Hello World</div>
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // TODO: Render the `App` component to the DOM
 import App from './components/App.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+import movies from './data/movies.js'
+
+ReactDOM.render(<App movies={movies}/>, document.getElementById('app'));
